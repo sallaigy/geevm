b5445f078e5f545c40e5c16bb84fc3f88c3425bc
Gyula Sallai <sallaigy@proton.me>
2025-01-20 19:17:32 +0100

    [GC] Additional tests for (Scoped)GcRootRef

--- src/unit_tests/GarbageCollectorTest.cpp
+++ src/unit_tests/GarbageCollectorTest.cpp
@@ -26,6 +26,7 @@ protected:
   Vm mVm{VmSettings{}};
   JavaThread mThread;
   InstanceClass mHelloWorldClass;
+  Instance* mHelloWorldObject;
 };
 
 TEST_F(GarbageCollectorTest, allocate_and_pin_object)
@@ -52,12 +53,12 @@ TEST_F(GarbageCollectorTest, allocate_and_pin_object)
 
 TEST_F(GarbageCollectorTest, allocate_and_pin_array)
 {
+  // Prevent GC from running during test setup
+  gc().lockGC();
+
   types::JString className = u"[Lorg/geevm/tests/classfile/HelloWorld;";
   ArrayClass arrayClass{className, FieldType::parse(className).value()};
   auto array = mVm.heap().allocateArray<Instance*>(&arrayClass, 10);
-
-  // Prevent GC from running during test setup
-  gc().lockGC();
 
   std::vector<Instance*> createdObjects;
   for (size_t i = 0; i < 10; i++) {
@@ -86,9 +87,53 @@ TEST_F(GarbageCollectorTest, allocate_and_pin_array)
   }
 }
 
-TEST_F(GarbageCollectorTest, compare_scoped_ref_to_null)
+TEST_F(GarbageCollectorTest, compare_root_ref)
 {
-  // Prevent GC from running during test setup
+  gc().lockGC();
+  auto object = mVm.heap().allocate(&mHelloWorldClass);
+  gc().unlockGC();
+
+  GcRootRef<> ref1 = gc().pin(object).release();
+  GcRootRef<> ref2 = gc().pin(object).release();
+
+  ASSERT_TRUE(ref1 == ref2);
+  ASSERT_TRUE(ref1 == object);
+}
+
+TEST_F(GarbageCollectorTest, scoped_root_ref_move)
+{
+  auto object = mVm.heap().allocate(&mHelloWorldClass);
+
+  ScopedGcRootRef<> ref1 = gc().pin(object);
+  ASSERT_TRUE(ref1 == object);
+
+  ScopedGcRootRef<> moveConstructed = std::move(ref1);
+  ASSERT_TRUE(moveConstructed == object);
+
+  auto anotherObject = mVm.heap().allocate(&mHelloWorldClass);
+
+  ScopedGcRootRef<> moveAssigned = gc().pin(anotherObject);
+  moveAssigned = std::move(moveConstructed);
+
+  ASSERT_TRUE(moveAssigned == object);
+}
+
+TEST_F(GarbageCollectorTest, compare_root_ref_to_null)
+{
+  auto object = mVm.heap().allocate(&mHelloWorldClass);
+
+  auto pinned = gc().pin(object).release();
+
+  ASSERT_FALSE(pinned == nullptr);
+
+  GcRootRef<> nullRef = nullptr;
+  ASSERT_TRUE(nullRef == nullptr);
+
+  ASSERT_FALSE(pinned == nullRef);
+}
+
+TEST_F(GarbageCollectorTest, compare_scoped_root_ref_to_null)
+{
   gc().lockGC();
   auto object = mVm.heap().allocate(&mHelloWorldClass);
   gc().unlockGC();
